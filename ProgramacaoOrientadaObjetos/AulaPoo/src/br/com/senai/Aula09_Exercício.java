ed6733a4ec1d5a34ae2cecd4ecc4bf11388d757e
Isadora Peixoto <133281545+isadoraapg@users.noreply.github.com>
2024-02-28 11:31:28 -0300

    Update Aula09_Exercício.java

--- ProgramacaoOrientadaObjetos/AulaPoo/src/br/com/senai/Aula09_Exercício.java
+++ ProgramacaoOrientadaObjetos/AulaPoo/src/br/com/senai/Aula09_Exercício.java
@@ -19,50 +19,81 @@ public class Aula09_Exercício {
 
 		//Critérios de avaliação:
 		//Identação, Oragnização, Apresentação do código, Execução do programa, Perguntas, Refazer uma parte, Funcionalidades.
-		
-		
+
+
 		int opcaoMesario = 0;
 		int opcaoEleitor = 0;
-		
+		int totalAngela = 0;
+		int totalGean = 0;
+		int totalCesar = 0;
+		int totalNulo = 0;
+		int totalBranco = 0;
+
+
 		do {
-			opcaoMesario = Integer.parseInt(JOptionPane.showInputDialog("Digite uma opcão: \n1) Abrir menu de votação. \n2)Sair."));
+			opcaoMesario = Integer.parseInt(JOptionPane.showInputDialog("Digite uma opcão: \n1) Abrir menu de votação. \n2)Encerrar."));
 			if(opcaoMesario ==1) {
-				do {
-					opcaoEleitor = Integer.parseInt(JOptionPane.showInputDialog("Digite sua opção de voto: \nCódigo: 1 \nNome: Angela Pepino \nPartido: PMDB \n\nCódigo: 2 \nNome: Gean da Silva \nPartido: PT \n\nCódigo: 3 \nNome: Cesar Souza Neto \nPartido: DEM \n\nCódigo: 4 \nNome: Voto Nulo \n\nCódigo: 5 \nNome: Voto Branco"));
-												
-							switch( opcaoEleitor ) {
-							
-							case 1:
-								System.out.println("Você votou na candidata: Angela Pepino - PMDB");
-							break;
-							
-							case 2:
-								System.out.println("Você votou no candidato: Gean da Silva - PT");
-							break;
-								
-							case 3:
-								System.out.println("Você votou no candidato: Cesar Souza Neto - DEM");
-							break;
-							
-							case 4:
-								System.out.println("Você votou NULO");
-							break;
-							
-							case 5:
-								System.out.println("Você votou BRANCO");
-							break;
-							default:
-								System.out.println("Opção inválida");
-							}
-				}while (opcaoEleitor != 1); //verificar 
-				
-		}}while (opcaoMesario != 2);
-		
-		System.out.println("Programa finalizado.");
-		
-		
-		
-		
-	}
+				opcaoEleitor = Integer.parseInt(JOptionPane.showInputDialog("Digite sua opção de voto: \nCódigo: 1 \nNome: Angela Pepino \nPartido: PMDB \n\nCódigo: 2 \nNome: Gean da Silva \nPartido: PT \n\nCódigo: 3 \nNome: Cesar Souza Neto \nPartido: DEM \n\nCódigo: 4 \nNome: Voto Nulo \n\nCódigo: 5 \nNome: Voto Branco"));
+
+				switch( opcaoEleitor ) {
+
+				case 1:
+					System.out.println("Voto: Angela Pepino - PMDB");
+					totalAngela++;
+					break;
+
+				case 2:
+					System.out.println("Voto: Gean da Silva - PT");
+					totalGean++;
+					break;
+
+				case 3:
+					System.out.println("Voto: Cesar Souza Neto - DEM");
+					totalCesar++;
+					break;
+
+				case 4:
+					System.out.println("Voto Nulo");
+					totalNulo++;
+					break;
+
+				case 5:
+					System.out.println("Voto Branco");
+					totalBranco++;
+					break;
+				default:
+					System.out.println("Opção inválida");
+				}
+
+			}else if(opcaoMesario != 1 && opcaoMesario!= 2) {
+				System.out.println("Opção inválida");
+			}
+		}while (opcaoMesario != 2);
+
+		System.out.println("\nVOTAÇÃO ENCERRADA.");
+		System.out.println("\nVotos Computados:");
+		System.out.println("Angela Pepino - PMDB: " + totalAngela);
+		System.out.println("Gean da Silva - PT: " + totalGean);
+		System.out.println("Cesar Souza Neto - DEM: " + totalCesar);
+		System.out.println("Nulo: " + totalNulo);
+		System.out.println("Branco: " + totalBranco);
+
+		if(totalAngela > totalGean && totalAngela > totalCesar && totalAngela > totalNulo && totalAngela > totalBranco) {
+			System.out.println("\nCandidato(a) mais votado(a): Angela Pepino - PMDB");
+		}else if(totalGean > totalAngela && totalGean > totalCesar && totalGean > totalNulo && totalGean > totalBranco) {
+			System.out.println("\nCandidato(a) mais votado(a): Gean da Silva - PT");
+		}else if(totalCesar > totalAngela && totalCesar > totalGean && totalCesar > totalNulo && totalCesar > totalBranco) {
+			System.out.println("\nCandidato(a) mais votado(a): Cesar Souza Neto - DEM");
+		}else if(totalNulo > totalAngela && totalNulo > totalCesar && totalNulo > totalGean && totalNulo > totalBranco) {
+			System.out.println("\nCandidato(a) mais votado(a): Nulo");
+		}else if(totalBranco > totalAngela && totalBranco > totalCesar&& totalBranco > totalNulo && totalBranco > totalGean) {
+			System.out.println("\nCandidato(a) mais votado(a): Branco");
+		}else {
+			System.out.println("Empate. Realizar votação novamente");
 		}
-	
+
+
+
+
+	}
+}
